28d5f60064120f443e1ba952fa1beeaaab68687e
Braxton Coats <coatsbr@appstate.edu>
2019-11-20 14:01:44 -0500

    button change

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -27,7 +27,7 @@ const Button = styled.button`
   border-color: ${get('colors.primary.default')};
 
   color: ${get('colors.text.inverted')};
-  background: ${get('colors.primary.dark')};
+  background: ${get('colors.primary.default')};
 
   padding: 12px 28px 10px 28px;
   display: inline-block;
